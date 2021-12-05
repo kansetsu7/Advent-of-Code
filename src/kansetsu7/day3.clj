ffd6a6bf817fc27f6dca0b4a02640c28823b0bf0
kansetsu7 <kansetsu4@gmail.com>
2021-12-05 20:20:12 +0800

    Day3 part2

--- src/kansetsu7/day3.clj
+++ src/kansetsu7/day3.clj
@@ -13,31 +13,73 @@
   (->> (cs/split (slurp (io/resource "day3.txt")) #"\n")
        (map #(-> % seq chars->int))))
 
-(defn count-zero
-  [zero-counts binaries]
-  (map #(cond-> %1 (zero? %2) inc) zero-counts binaries))
+(defn map-nth
+  [coll position]
+  (map #(nth % position) coll))
 
-(defn most-common-num
-  [lines zero-counts]
-  (if (> zero-counts (/ lines 2)) 0 1))
+(defn get-most-common
+  [nums]
+  (let [grouped-num (group-by identity nums)
+        zero-count  (count (get grouped-num 0))
+        one-count   (count (get grouped-num 1))]
+    (if (>= one-count zero-count) 1 0)))
 
-(defn ->decimal
-  [[power bi-digit]]
-  (* bi-digit (math/expt 2 power)))
+(defn get-most-common-at
+  [nums position]
+  (->> (map-nth nums position)
+       get-most-common))
+
+(defn revert-bit
+  [bit]
+  (if (zero? bit) 1 0))
+
+(defn revert-bits
+  [binary]
+  (map revert-bit binary))
+
+(defn bit-power->decimal
+  [[power bit]]
+  (* bit (math/expt 2 power)))
 
 (defn binary->deciaml
   [binary]
   (->> (reverse binary)
        (map-indexed vector)
-       (reduce #(+ %1 (->decimal %2)) 0)))
+       (reduce #(+ %1 (bit-power->decimal %2)) 0)))
+
+(defn filter-at-position
+  [binary-coll rate-type position]
+  (let [criteria (cond-> (get-most-common-at binary-coll position)
+                   (= rate-type :CO2) revert-bit)]
+    (filter #(= criteria (nth % position)) binary-coll)))
+
+(defn get-rating
+  [input rate-type]
+  (loop [binary-coll input
+         position    0]
+    (if (= 1 (count binary-coll))
+      (-> binary-coll first binary->deciaml)
+      (recur (filter-at-position binary-coll rate-type position)
+             (inc position)))))
 
 (comment
   ;; part1: 3959450
   (let [input (puzzle-input)
-        lines (count input)
-        gamma-binary (->> input
-                          (reduce count-zero (repeat 0))
-                          (map #(most-common-num lines %)))
-        epsilon-binary (map #(if (zero? %) 1 0) gamma-binary)]
+        bits (-> input first count)
+        gamma-binary (map #(get-most-common-at input %) (range bits))
+        epsilon-binary (revert-bits gamma-binary)]
     (* (binary->deciaml gamma-binary)
-       (binary->deciaml epsilon-binary))))
+       (binary->deciaml epsilon-binary)))
+
+  ;; part2: 7440311
+  (let [input (puzzle-input)]
+    (* (get-rating input :CO2)
+       (get-rating input :O2))))
+
+  ;; for debug
+  ;; "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010"
+  ;; (defn puzzle-input-2
+  ;;   [input]
+  ;;   (->> (cs/split input #"\n")
+  ;;        (map #(-> % seq chars->int)))))
+
