eb4ebba13b27dda9997b253e05dce9cbe968a3ad
kansetsu7 <kansetsu4@gmail.com>
2021-12-09 08:22:07 +0800

    Day8 part2

--- src/kansetsu7/2021/day8.clj
+++ src/kansetsu7/2021/day8.clj
@@ -1,9 +1,8 @@
 (ns kansetsu7.day8
   (:require
     [clojure.java.io :as io]
-    [clojure.string :as cs]
-    [kansetsu7.util :as util]
-    [clojure.math.numeric-tower :as math]))
+    [clojure.math.numeric-tower :as math]
+    [clojure.set :as cset]))
 
 (def example-data
   "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
@@ -39,52 +38,108 @@
   (->> (take-nth 2 (rest input))
        flatten))
 
-(defn count-uniq-output
-  [input]
-  (->> (extract-output input)
-       count-segments
-       (filter #(uniq-num-segs->num %))
-       count))
-
 (defn ->segments
   [string]
   (set (re-seq #"\w" string)))
 
-(def seg-num-map
-  (->> ["cagedb"
-        "ab"
-        "gcdfa"
-        "fbcad"
-        "eafb"
-        "cdfbe"
-        "cdfgeb"
-        "dab"
-        "acedgfb"
-        "cefabd"]
-       (map #(-> % ->segments))
-       (map-indexed #(vector %2 %1))
-       (into {})))
-         ;; {(->segments "cdfeb") 5
-         ;;  (->segments "fcadb") 3
-         ;;  (->segments "cdbaf") 3}))
+(defn count-uniq-output
+  [input]
+  (->> (take-nth 2 (rest input))
+       flatten
+       (filter #(-> % ->segments count uniq-num-segs->num))
+       count))
 
-(defn segment->num
-  [seg]
-  (or (seg-num-map seg)
-      (-> seg count uniq-num-segs->num)))
+(defn ->uniq-segment-map
+  [seg-num]
+  (->> (map ->segments seg-num)
+       (map #(hash-map :segment % :count (count %) :num (-> % count uniq-num-segs->num)))))
+
+
+(defn ->known-num-segs
+  [seg-nums]
+  (->> (map ->segments seg-nums)
+       (map #(vector % (-> % count uniq-num-segs->num)))
+       (filter last)
+       (into {})))
+
+(defn count5-seg->num
+  [seg known-num->seg]
+  (let [num4 (known-num->seg 4)
+        num1 (known-num->seg 1)]
+    (cond
+      (cset/superset? seg num1) 3
+      (cset/superset? seg (cset/difference num4 num1)) 5
+      :else 2)))
+
+(defn count6-seg->num
+  [seg known-num->seg]
+  (let [num7 (known-num->seg 7)
+        num4 (known-num->seg 4)]
+    (cond
+      (not (cset/superset? seg num7)) 6
+      (cset/superset? seg num4) 9
+      :else 0)))
+
+(defn unknow-seg->num
+  [seg known-num-segs]
+  (let [known-num->seg #((cset/map-invert known-num-segs) %)]
+    (if (= 5 (count seg))
+      (count5-seg->num seg known-num->seg)
+      (count6-seg->num seg known-num->seg))))
+
+(defn seg->num
+  [seg-num known-num-segs]
+  (let [seg (->segments seg-num)]
+    (if (known-num-segs seg)
+      (known-num-segs seg)
+      (unknow-seg->num seg known-num-segs))))
+
+(defn seg-num-mapping
+  [seg-nums]
+  (let [known-num-segs (->known-num-segs seg-nums)]
+    (->> seg-nums
+         (map #(vector (->segments %) (seg->num % known-num-segs)))
+         (into {}))))
+
+(defn four-digits-val
+  [digits]
+  (->> (vec digits)
+       reverse
+       (map-indexed vector)
+       (map (fn [[p n]] (* n (math/expt 10 p))))
+       (apply +)))
+
+(defn cal-output-nums
+  [[ten-nums-segs output-segs]]
+  (let [->num (seg-num-mapping ten-nums-segs)]
+    (->> output-segs
+         (map ->segments)
+         (map ->num)
+         four-digits-val)))
+
+(defn sum-output-nums
+  [input]
+  (->> (partition 2 input)
+       (map cal-output-nums)
+       (apply +)))
+
+;; part1: 369
+(defn part1
+  []
+  (count-uniq-output (puzzle-input)))
+
+;; 1031553
+(defn part2
+  []
+  (sum-output-nums (puzzle-input)))
 
 (comment
   ;; example
   (let [input (puzzle-input example-data)]
+    ;; part1
     (count-uniq-output input)
-    (->> (extract-output input)
-         (map ->segments)
-         (map #(vector % (segment->num %)))))
+    ;; part2
+    (sum-output-nums input))
 
-  ;; part1: 369
-  (let [input (puzzle-input)]
-    (count-uniq-output input)))
-
-  ;; part2:
-  ;; (let [input (puzzle-input)
-  ;;       ]))
+  (part1)
+  (part2))
